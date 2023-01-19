6884da53aa72c93690f16556d1f54391ff804881
Christian McVerry <cmcverry@gmail.com>
2023-01-19 18:57:42 -0500

    Added missing content to portfolio, fixed some typos, and implemented some minor style changes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -16,15 +16,19 @@ function About(props) {
                             <Card.Header><h2>Background</h2></Card.Header>
                             <Card.Body>
                                 <Card.Text>
-                                        My name is Christian McVerry. I have a varied background with both a strong technical and qualitative skill set. I hold two bachelors degrees: 
-                                        one in Computer Science from Oregon State University and another in History from Skidmore College. I enjoy learning new things, 
-                                         developing software, web applications, experimenting with new frameworks and APIs, and generally dabbling  with technologies that capture my interest.
+                                        My name is Christian McVerry. I have a unique background that has granted me both a strong technical and qualitative skill set. I hold two bachelor's degrees: 
+                                        one in <span className="highlight-text">Computer Science from Oregon State University </span> and another in <span className="highlight-text"> History from Skidmore College </span> . I have been successful in both a <span className="highlight-text">technical</span> and <span className="highlight-text">liberal arts</span> learning environment, 
+                                        which highlights my <span className="highlight-text">adaptability </span> and <span className="highlight-text">breadth</span>. And I am always <span className="highlight-text">ready to learn and try new things</span>. It is a passion of mine regardless of
+                                        whether I am learning through a book, article, video, developer documentation, and or just plain tinkering on my own. Equally important, I love putting what I learn into practice. Over the past few years,
+                                         alongside completing my degree in Computer Science, <span className="highlight-text"> I have developed my own software applications, learned new programming frameworks and APIs, and generally dabbled with technologies that 
+                                         have captured my interest</span>.
                                     <br/>
                                     <br/>
-                                        Unrelated to the tech side of me, I also have heaps of gardening experience, which is another interest of mine. Over the years I have spent time
-                                        volunteering in various public gardens and even worked as a professional rose gardener for about three years. I have worked as a private contractor as well as an employee at 
+                                        Perhaps unconventional for a young developer, I have well over a decade of gardening experience that spans all the way back to my time in high school. Over the years, I <span className="highlight-text">managed</span> a high school greenhouse,
+                                        <span className="highlight-text"> volunteered</span> in various public gardens, and even <span className="highlight-text">worked as a professional rose gardener</span> for about three years. In those three years, I worked as a private contractor as well as an employee at 
                                         the Elizabeth Park Conservancy where I was a steward to the oldest and the third largest public rose garden in the United States. My time working as a professional
-                                        gardener further developed my leadership, qualitative, and soft skills. While in recent years software development has been my primary endeavor, I do spend time gardening as a hobby.
+                                        gardener further developed my <span className="highlight-text">leadership</span>, qualitative, and soft skills. But more than anything else, gardening has reinforced in me the value of <span className="highlight-text">diligence, patience, and humility</span>. 
+                                        While more recently <span className="highlight-text">software development has been my primary endeavor</span>, I do spend time gardening as a hobby.
                                 </Card.Text>
                             </Card.Body>
                         </Card>   
