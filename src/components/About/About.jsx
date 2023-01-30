778d1765ea731905ac253a43c8661764540f58b0
cmcverry <cmcverry@gmail.com>
2023-01-30 18:12:28 -0500

    edits to content

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -16,19 +16,30 @@ function About(props) {
                             <Card.Header><h2>Background</h2></Card.Header>
                             <Card.Body>
                                 <Card.Text>
-                                        My name is Christian McVerry. I have a unique background that has granted me both a strong technical and qualitative skill set. I hold two bachelor's degrees: 
-                                        one in <span className="highlight-text">Computer Science from Oregon State University </span> and another in <span className="highlight-text"> History from Skidmore College </span> . I have been successful in both a <span className="highlight-text">technical</span> and <span className="highlight-text">liberal arts</span> learning environment, 
-                                        which highlights my <span className="highlight-text">adaptability </span> and <span className="highlight-text">breadth</span>. And I am always <span className="highlight-text">ready to learn and try new things</span>. It is a passion of mine regardless of
-                                        whether I am learning through a book, article, video, developer documentation, and or just plain tinkering on my own. Equally important, I love putting what I learn into practice. Over the past few years,
-                                         alongside completing my degree in Computer Science, <span className="highlight-text"> I have developed my own software applications, learned new programming frameworks and APIs, and generally dabbled with technologies that 
-                                         have captured my interest</span>.
+                                        My name is Christian McVerry. I have a wide background that has granted me both a strong technical and qualitative skill set. 
+                                        I hold two bachelor's degrees: one in <span className="highlight-text">Computer Science from Oregon State University </span> and another 
+                                        in <span className="highlight-text"> History from Skidmore College</span>. I have been successful in both 
+                                        <span className="highlight-text"> technical</span> and <span className="highlight-text">liberal arts</span> learning environments, which 
+                                        demonstrates my <span className="highlight-text">adaptability</span>, <span className="highlight-text">breadth</span>, and 
+                                        <span className="highlight-text"> openness to learning</span>. I am always <span className="highlight-text">ready to learn 
+                                        and try new things</span>. It is a passion of mine regardless of
+                                        whether I am learning through a book, article, video, documentation, or just plain tinkering on my own. Equally 
+                                        important, I love putting what I learn into practice. Over the past few years, alongside completing my degree in 
+                                        Computer Science, <span className="highlight-text"> I have developed my own software applications, learned new programming 
+                                        frameworks and APIs, and generally dabbled with technologies that have captured my interest</span> like building a Raspberry Pi arcade 
+                                        console and diving into the Linux ecosystem.
                                     <br/>
                                     <br/>
-                                        Perhaps unconventional for a young developer, I have well over a decade of gardening experience that spans all the way back to my time in high school. Over the years, I <span className="highlight-text">managed</span> a high school greenhouse,
-                                        <span className="highlight-text"> volunteered</span> in various public gardens, and even <span className="highlight-text">worked as a professional rose gardener</span> for about three years. In those three years, I worked as a private contractor as well as an employee at 
-                                        the Elizabeth Park Conservancy where I was a steward to the oldest and the third largest public rose garden in the United States. My time working as a professional
-                                        gardener further developed my <span className="highlight-text">leadership</span>, qualitative, and soft skills. But more than anything else, gardening has reinforced in me the value of <span className="highlight-text">diligence, patience, and humility</span>. 
-                                        While <span className="highlight-text"> more recently software development has been my primary endeavor</span>, I do spend time gardening as a hobby.
+                                        Another interesting thing about me, perhaps unconventional for a young software developer, is that I have well over a decade of gardening 
+                                        experience that spans all the way back to my time in high school. Over the years, I <span className="highlight-text">managed </span> 
+                                        a high school greenhouse, <span className="highlight-text"> volunteered</span> in various public gardens, and 
+                                        even <span className="highlight-text">worked as a professional rose gardener</span> for about three years. In those three years, 
+                                        I worked as a private contractor as well as an employee at the Elizabeth Park Conservancy where I was a steward to the oldest and the 
+                                        third largest public rose garden in the United States. My time working as a professional gardener further developed my
+                                         <span className="highlight-text"> leadership</span>, qualitative, and soft skills. But more than anything else, gardening has 
+                                         reinforced in me the value of <span className="highlight-text">diligence, patience, and humility</span>. 
+                                         While <span className="highlight-text"> more recently software development has been my primary endeavor</span>, I do spend 
+                                         time gardening as a hobby.
                                 </Card.Text>
                             </Card.Body>
                         </Card>   
@@ -63,20 +74,23 @@ function About(props) {
                             <Card.Header><h2>Education</h2></Card.Header>
                             <Card.Body>
                                     <h4>
-                                        Bachelor of Science, Computer Science
+                                        B.S. Computer Science
                                         <br/>
                                         June 2022 
                                         <br/>
                                         Oregon State University - Corvallis, OR
+                                        <br/>
+                                        <h5 className="gpa">GPA: 3.98</h5>
                                     </h4>
                                         <br/>
                                     <h4>
-                                        Bachelor of Arts, History
+                                        B.A. History
                                         <br/>
                                         May 2018
                                         <br/>
                                         Skidmore College - Saratoga Springs, NY 
                                         <br/>
+                                        <h5 className="gpa">GPA: 3.3</h5>
                                     </h4>
                             </Card.Body>
                         </Card>
@@ -87,16 +101,16 @@ function About(props) {
                         <Card id="" >
                             <Card.Header><h2>Technical</h2></Card.Header>
                             <Card.Body>
-                                    <p style={{color: 'orange', fontStyle: 'italic'}} >( Note: this list contains what I use frequently or have the most exposure with. Not necessarily 
-                                    full mastery. )</p>
+                                    <p style={{color: 'orange', fontStyle: 'italic'}} >Note: this list contains what I have the most exposure to and use frequently. Not necessarily 
+                                    full technical mastery</p>
                                     <h5>Programming Languages:</h5>
-                                    <p>Python, JavaScript, C/C++, SQL, HTML/CSS</p>
+                                    <p>Python, C, C++, SQL, JavaScript, HTML, CSS</p>
                                     <h5>Libraries / Frameworks / APIs:</h5>
                                     <p>ReactJS, NodeJS, ExpressJS, React Native, Flask, OpenMP, OpenCL</p>
                                     <h5>Environments / Tools / Platforms:</h5>
-                                    <p>Unix/Linux, Git, Google Cloud Platform, Microsoft Azure, Firebase, Figma, Asana, Visual Studio, VS Code, CLion</p>
+                                    <p>Linux/Unix, Git, Google Cloud Platform, Firebase, Microsoft Azure, Figma, Asana, Visual Studio, VS Code, CLion</p>
                                     <h5>Conceptual:</h5>
-                                    <p>Object-Oriented Programming, Parallel Proramming, RESTful API, Microservice Architecture, Agile Concepts & Practices</p>
+                                    <p>Object-Oriented Programming, Parallel Programming, REST API, Microservice Architecture, Agile Concepts & Practices</p>
                             </Card.Body>
                         </Card>
                     </Col>
